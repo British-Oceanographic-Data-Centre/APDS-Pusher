2e692db4068620a0d8059828da697e1c8ec47d53
Andrew Hale <andhal@bodc.ac.uk>
2022-05-04 08:55:54 +0000

    BODCDMSYS-4334: load configuration file

---
+++ apds_pusher/config_parser.py
@@ -0,0 +1,53 @@
+"""Parser for APDS pusher configuration files."""
+from dataclasses import dataclass, fields
+from typing import List
+
+
+class ParserException(Exception):
+    """Base exception for the APDS pusher."""
+
+
+class ExtraFieldError(ParserException):
+    """Exception raised when an additional (unexpected) field is given."""
+
+
+class MissingFieldError(ParserException):
+    """Exception raised when missing a required field."""
+
+
+class BlankValueError(ParserException):
+    """Exception raised when the value for a field is blank."""
+
+
+@dataclass
+class Configuration:
+    """Dataclass to hold configuration."""
+
+    auth0_tenant: str
+    client_id: str
+    bodc_archive_url: str
+    file_formats: List[str]
+    archive_checker_frequency: int
+
+    @classmethod
+    def from_dict_validated(cls, data_dict):
+        """Instantiate the class from a dictionary."""
+        class_fields = {field.name for field in fields(cls)}
+        data_fields = set(data_dict.keys())
+
+        # check no additional fields
+        extra_keys = data_fields - class_fields
+        if extra_keys:
+            raise ExtraFieldError(f"Unexpected fields given: {extra_keys}")
+
+        # check no missing fields
+        missing_keys = class_fields - data_fields
+        if missing_keys:
+            raise MissingFieldError(f"Missing expected fields: {missing_keys}")
+
+        # check no blank values
+        blank_fields = {key for key, value in data_dict.items() if not value}
+        if blank_fields:
+            raise BlankValueError(f"Blank values found for fields: {blank_fields}")
+
+        return cls(**data_dict)
---
+++ tests/test_cli.py
@@ -0,0 +1,36 @@
+"""Test CLI."""
+from pathlib import Path
+
+import click
+import pytest
+
+from apds_pusher import main, config_parser
+
+
+@pytest.fixture(name="config_path")
+def config_path_fixture():
+    """A fixture containing a path to a valid configuration JSON file."""
+    return Path(__file__).parent / "example_config.json"
+
+
+def test_load_configuration_file(config_path):
+    """Check that the correct data is returned from a call to load the config file."""
+    config = main.load_configuration_file(config_path)
+
+    assert isinstance(config, config_parser.Configuration)
+
+
+@pytest.mark.parametrize(
+    "exception_type",
+    [
+        config_parser.ExtraFieldError,
+        config_parser.MissingFieldError,
+        config_parser.BlankValueError,
+    ],
+)
+def test_click_exception_on_parse_error(mocker, config_path, exception_type):
+    """Check that the correct exception is raised when a parsing error occurs."""
+    mocker.patch.object(config_parser.Configuration, "from_dict_validated", side_effect=exception_type("an exception"))
+
+    with pytest.raises(click.ClickException):
+        main.load_configuration_file(config_path)
